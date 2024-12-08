73e87e497399116d38f43c3e1059d26542709029
Andrei <soprachev@mail.ru>
2024-12-08 17:42:05 +0300

    add new metatags

--- lib/sdk/metaTags.ts
+++ lib/sdk/metaTags.ts
@@ -3,7 +3,13 @@ const META_PREFIX = 'wotstat-widget:'
 
 function setMeta(name: string, content: string = 'true') {
   const prefixedName = META_PREFIX + name
-  if (document.head.querySelector(`meta[name="${prefixedName}"]`)) return
+  if (document.head.querySelector(`meta[name="${prefixedName}"]`)) {
+    if (content === 'true') return
+    const meta = document.head.querySelector(`meta[name="${prefixedName}"]`) as HTMLMetaElement
+    meta.content = content
+    return
+  }
+
   const meta = document.createElement('meta')
   meta.name = prefixedName
   meta.content = content
@@ -36,18 +42,55 @@ export namespace MetaTags {
     else removeMeta('use-sniper-mode')
   }
 
+  export function setPreferredTopLayer(enabled: boolean) {
+    if (enabled) setMeta('preferred-top-layer')
+    else removeMeta('preferred-top-layer')
+  }
+
+  export function setUnlimitedSize(enabled: boolean) {
+    if (enabled) setMeta('unlimited-size')
+    else removeMeta('unlimited-size')
+  }
+
+  export function setInsets(value: false | string | number | { top?: number, right?: number, bottom?: number, left?: number }) {
+    if (typeof value === "object") {
+      const { top = 0, right = 0, bottom = 0, left = 0 } = value
+      value = `${top} ${right} ${bottom} ${left}`
+    } else if (typeof value === "number") {
+      value = `${value} ${value} ${value} ${value}`
+    } else if (typeof value === "string") {
+      const splitted = value.trim().split(/\s\s*/)
+      const values = splitted.map(v => parseFloat(v))
+      if (values.length == 1) {
+        value = `${values[0]} ${values[0]} ${values[0]} ${values[0]}`
+      } else if (values.length == 2) {
+        value = `${values[0]} ${values[1]} ${values[0]} ${values[1]}`
+      } else if (values.length == 3) {
+        value = `${values[0]} ${values[1]} ${values[2]} ${values[1]}`
+      } else if (values.length == 4) {
+        value = `${values[0]} ${values[1]} ${values[2]} ${values[3]}`
+      }
+    }
+
+    console.log(value);
+
+
+    if (value === false || value === '0 0 0 0') removeMeta('insets')
+    else setMeta('insets', value)
+  }
+
   /**
    * @deprecated This function is deprecated. Use setAutoHeight(true) instead.
    */
   export function enableAutoHeight() {
-    setMeta('wotstat-widget:auto-height')
+    setMeta('auto-height')
   }
 
   /**
    * @deprecated This function is deprecated. Use setAutoHeight(false) instead.
    */
   export function disableAutoHeight() {
-    removeMeta('wotstat-widget:auto-height')
+    removeMeta('auto-height')
   }
 
 }
